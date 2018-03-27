1b0428f4089d37b337c25f6d50bb30dd16eeeee5
Tuan Dat <tuandatqn95@gmail.com>
2018-03-27 23:19:30 +0700

    authenticate drive

--- src/main/java/com/example/storage/DriveService.java
+++ src/main/java/com/example/storage/DriveService.java
@@ -1,13 +1,9 @@
 package com.example.storage;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -18,10 +14,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.model.DriveFile;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.media.MediaHttpUploader;
 import com.google.api.client.http.FileContent;
@@ -57,7 +50,7 @@ public class DriveService {
 	 * If modifying these scopes, delete your previously saved credentials at
 	 * ~/.credentials/drive-java-quickstart
 	 */
-	private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
+	// private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
 
 	static {
 		try {
@@ -75,16 +68,29 @@ public class DriveService {
 	 * @return an authorized Credential object.
 	 * @throws IOException
 	 */
+	// public static Credential authorize() throws IOException {
+	// // Load client secrets.
+	// InputStream in =
+	// DriveService.class.getResourceAsStream("/client_secret.json");
+	// GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+	// new InputStreamReader(in));
+	//
+	// // Build flow and trigger user authorization request.
+	// GoogleAuthorizationCodeFlow flow = new
+	// GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+	// clientSecrets,
+	// SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
+	// Credential credential = new AuthorizationCodeInstalledApp(flow, new
+	// LocalServerReceiver()).authorize("user");
+	// System.out.println("Credentials saved to " +
+	// DATA_STORE_DIR.getAbsolutePath());
+	// return credential;
+	// }
 	public static Credential authorize() throws IOException {
-		// Load client secrets.
-		InputStream in = DriveService.class.getResourceAsStream("/client_secret.json");
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+		GoogleCredential credential = GoogleCredential
+				.fromStream(DriveService.class.getResourceAsStream("/My Project-d80184c3ef0f.json"))
+				.createScoped(Collections.singleton(DriveScopes.DRIVE));
 
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
-		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
 		return credential;
 	}
 
